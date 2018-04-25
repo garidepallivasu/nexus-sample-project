2d972d00ca6154799b277e9e3a401f2c65d20dfc
Carlo Cayos <carlocayos@gmail.com>
2018-04-25 17:07:12 +1000

    Initial commit

---
+++ src/main/java/io/proximax/nexussampleproject/nexussampleproject/NexusSampleProjectApplication.java
@@ -0,0 +1,12 @@
+package io.proximax.nexussampleproject.nexussampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusSampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusSampleProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/io/proximax/nexussampleproject/nexussampleproject/NexusSampleProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package io.proximax.nexussampleproject.nexussampleproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NexusSampleProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
